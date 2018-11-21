d57fa04e0432c96dc725d234c7bfd1e44accc25e
Dagh Zeppenfeld <daghzep@gmail.com>
2018-11-21 23:01:08 +0100

    Initial sharing of project

---
+++ SuMAnwendungTest.java
@@ -0,0 +1,39 @@
+
+/**
+ * Die Test-Klasse SuMAnwendungTest.
+ *
+ * @author 
+ * @version 
+ */
+public class SuMAnwendungTest extends junit.framework.TestCase
+{
+    // Bezugsobjekte
+
+    // Attribute
+
+    // Konstruktor
+    public SuMAnwendungTest()
+    {
+    }
+
+    /**
+     *  Setzt die Festlegungen fuer den Test.
+     *
+     * Wird vor jeder Testfall-Methode aufgerufen.
+     */
+    protected void setUp()
+    {
+    }
+
+    /**
+     * Gibt die Test-Festlegungen wieder frei.
+     *
+     * Wird nach jeder Testfall-Methode aufgerufen.
+     */
+    protected void tearDown()
+    {
+    }
+
+    // Testmethoden
+
+}
